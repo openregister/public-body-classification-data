308d5e4fef3fd69f8121e7c6321d0feaf11f214f
Duncan Garmonsway <nacnudus@gmail.com>
2017-08-22 11:08:34 +0100

    Write R script to download various org registers for fuzzy matching

---
+++ R/download-organisations.R
@@ -0,0 +1,29 @@
+# Download and combine the various registers of public bodies,
+# currently:
+# * government-organisation
+# * local-authority-eng
+# * local-authority-sct
+# * principal-local-authority
+
+library(RegistersClientR)
+library(tidyverse)
+library(here)
+
+org <-
+  rr_records("government-organisation") %>%
+  mutate(organisation = paste0("government-organisation:", `government-organisation`))
+eng <-
+  rr_records("local-authority-eng") %>%
+  mutate(organisation = paste0("local-authority-eng:", `local-authority-eng`))
+sct <-
+  rr_records("local-authority-sct") %>%
+  mutate(organisation = paste0("local-authority-sct:", `local-authority-sct`))
+wls <-
+  rr_records("principal-local-authority") %>%
+  mutate(organisation = paste0("principa-local-authority:", `principal-local-authority`))
+
+bind_rows(eng, sct, wls) %>%
+  select(`official-name`, organisation) %>%
+  rename("name" = `official-name`) %>%
+  bind_rows(select(org, name, organisation)) %>%
+  write_tsv(file.path(here(), "lists", "organisation.tsv"))
