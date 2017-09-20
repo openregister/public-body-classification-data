5a69e4de46a8a421354a810eea67e6260f296496
Duncan Garmonsway <nacnudus@gmail.com>
2017-09-20 13:35:38 +0100

    Set dates with resolution of one day

--- R/final-clean.R
+++ R/final-clean.R
@@ -1,4 +1,5 @@
 # Write NA as blank
+# Write dates with resolution of one day
 # Remove names when the organisation field is already populated
 
 library(tidyverse)
@@ -7,6 +8,8 @@ library(here)
 public_body_path <- here("data", "public-body.tsv")
 
 read_tsv(public_body_path) %>%
-  mutate(name = ifelse(!is.na(organisations), NA_character_, name)) %>%
+  mutate(name = ifelse(!is.na(organisations), NA_character_, name),
+         `start-date` = format(`start-date`, "%Y-%m-%d"),
+         `end-date` = format(`end-date`, "%Y-%m-%d")) %>%
   write_tsv(public_body_path, na = "")
 
